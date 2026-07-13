3e609c832da503f4b7349b221313682c0fdde764
69070077 <69070077@kmitl.ac.th>
2026-07-14 01:18:40 +0700

    submit

---
+++ 3006.py
@@ -0,0 +1,8 @@
+"""Gift"""
+def main():
+    """Gift"""
+    m = float(input())
+    k = float(input())
+    x = 2*m - k #เข้าสมการหาค่าเฉลี่ย จากนั้นสลับฝั่งสมการเผื่อหา x *สมการ*>>>>(x+k)/ 2 = m
+    print(x)
+main()
--- 3010.py
+++ 3010.py
@@ -3,12 +3,18 @@ def main():
     """Quadrant"""
     x = int(input())
     y = int(input())
-    if x >= 0 and y >= 0:
+    if x == 0 == y:
+        print("O")
+    elif y != 0 == x:
+        print("Y")
+    elif x != 0 == y:
+        print("X")
+    elif x > 0 and y > 0:
         print("Q1")
-    elif x <= 0 and y > 0:
+    elif x < 0 < y:
         print("Q2")
-    elif x <= 0 and y <= 0:
-        print("Q1")
-    elif x > 0 and y <= 0:
+    elif x < 0 > y:
+        print("Q3")
+    elif x > 0 > y:
         print("Q4")
 main()
