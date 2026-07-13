fea270469b1215cad6795a9c1eefa17ff0bf7187
69070077 <69070077@kmitl.ac.th>
2026-07-14 01:37:40 +0700

    Create 3023.py

---
+++ 3023.py
@@ -0,0 +1,13 @@
+"""Calculator"""
+def main():
+    """Calculator"""
+    n = int(input())
+    if n == 1:
+        print(n)
+    elif n >= 1:
+        n *= 2
+        print(n)
+    elif n < 0:
+        n *= 2
+        print(n)
+main()
