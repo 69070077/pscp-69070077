726af2dfffe43730ea07ee54c85a859d6ecad81a
Thanawat Panthapalin <69070077@kmitl.ac.th>
2026-07-24 11:23:50 +0700

    update

--- 3066.py
+++ 3066.py
@@ -6,8 +6,12 @@ def main():
     num3 = int(input())
     if num1 == num2 == num3:
         print("all the same")
+    elif num1 == num2 and num1 != num3:
+        print("neither")
+    elif num1 == num3 and num1 != num2:
+        print("neither")
+    elif num2 == num3 and num2 != num1:
+        print("neither")
     elif num1 != num2 != num3:
         print("all different")
-    else:
-        print("neither")
 main()
