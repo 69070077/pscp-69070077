c7fb4052224a3d436fe95acfce129ac00bee9f05
69070303 <69070303@kmitl.ac.th>
2026-07-24 09:45:08 +0700

    Create 3066.py

---
+++ 3066.py
@@ -0,0 +1,13 @@
+"""Same"""
+def main():
+    """Same"""
+    num1 = int(input())
+    num2 = int(input())
+    num3 = int(input())
+    if num1 == num2 == num3:
+        print("all the same")
+    elif num1 != num2 != num3:
+        print("all different")
+    else:
+        print("neither")
+main()
