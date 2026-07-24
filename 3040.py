a7680a6db5a592f4699c87e8784252332a230a0b
Thanawat Panthapalin <69070077@kmitl.ac.th>
2026-07-24 12:36:50 +0700

    Create 3040.py

---
+++ 3040.py
@@ -0,0 +1,14 @@
+"""Exchange"""
+def main():
+    """Exchange"""
+    money = int(input())
+    sibbaht = money // 10
+    fivebaht 
+    fivebaht = sibbaht // 5
+    twobaht = fivebaht // 2
+    onebaht = twobaht // 1
+    print(f"10 = {sibbaht}")
+    print(f"5 = {fivebaht}")
+    print(f"2 = {twobaht}")
+    print(f"1 = {onebaht}")
+main()
