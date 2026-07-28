4da5e4cbfd73adccf90b290ed2e8bd990587d93c
69070077 <69070077@kmitl.ac.th>
2026-07-28 17:16:42 +0700

    Create 3030.py

---
+++ 3030.py
@@ -0,0 +1,34 @@
+"""Saitama"""
+def main():
+    """Saitama"""
+    pushup_need = int(input())
+    situp_need = int(input())
+    squat_need = int(input())
+    run_need = int(input())
+    pushup_cando = int(input())
+    situp_cando = int(input())
+    squat_cando = int(input())
+    run_cando = int(input())
+    day = []
+
+    daypush = pushup_need // pushup_cando
+    if pushup_need % pushup_cando:
+        daypush += 1
+    day.append(daypush)
+
+    daysitup = situp_need // situp_cando
+    if situp_need % situp_cando:
+        daysitup += 1
+    day.append(daysitup)
+
+    daysquat = squat_need // squat_cando
+    if squat_need % squat_cando:
+        daysquat += 1
+    day.append(daysquat)
+
+    dayrun = run_need // run_cando
+    if run_need % run_cando:
+        dayrun += 1
+    day.append(dayrun)
+    print(max(day))
+main()
