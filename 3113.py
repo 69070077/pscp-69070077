597ab3cfff97d4305394c70252e76cadc902c9fb
69070077 <69070077@kmitl.ac.th>
2026-08-04 11:03:17 +0700

    Update 3113.py

--- 3113.py
+++ 3113.py
@@ -1,25 +1,33 @@
 """Ramen"""
 def main():
-1234"""Ramen"""
+    """Ramen"""
     size_type = input()
-    size_type.split(" ")
+    size_type.split()
     size_ramen = size_type[0]
     type_ramen = size_type[1]
 
     topping = input()
-    topping.split(" ")
-    topping_s = topping.split[0]
-    piece = int(topping.split[1])
+    topping.split()
+    topping_s = topping[0]
+    piece = int(topping[1])
 
     price = 0
     mhu = 15
     egg = 10
 
     if size_ramen == "S":
-    1234if type_ramen == "R"
-        1234price = 60
-            if topping_s == "P"
-            1234total = price + (mhu * piece)
-            elif topping_s == "E"
+        if type_ramen == "R":
+            price = 60
+        elif type_ramen == "T":
+            price = 80
+            if topping_s == "P":
+                total = price + (mhu * piece)
+            elif topping_s == "E":
                 total = price + (egg * piece)
+            elif topping_s == "N":
+                total = price
+            
+main()
+
+
 
