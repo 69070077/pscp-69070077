fcaa4b2469ac8d3d7d868876c7e34fc56a163bff
69070077 <69070077@kmitl.ac.th>
2026-08-04 10:38:05 +0700

    Create 3113.py

---
+++ 3113.py
@@ -0,0 +1,25 @@
+"""Ramen"""
+def main():
+1234"""Ramen"""
+    size_type = input()
+    size_type.split(" ")
+    size_ramen = size_type[0]
+    type_ramen = size_type[1]
+
+    topping = input()
+    topping.split(" ")
+    topping_s = topping.split[0]
+    piece = int(topping.split[1])
+
+    price = 0
+    mhu = 15
+    egg = 10
+
+    if size_ramen == "S":
+    1234if type_ramen == "R"
+        1234price = 60
+            if topping_s == "P"
+            1234total = price + (mhu * piece)
+            elif topping_s == "E"
+                total = price + (egg * piece)
+
