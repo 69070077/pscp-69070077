240d71856c82fadc32256f083a7b1f4793aa409d
69070077 <69070077@kmitl.ac.th>
2026-08-07 16:08:58 +0700

    Update 3156.py

--- 3156.py
+++ 3156.py
@@ -3,7 +3,9 @@ def main():
     """Conan"""
     text = input()
     num = int(input())
+    result = ""
     for i in text:
-        ascii = ord(i)
-        ascii += num
-        print(chr(ascii), end="")
+        asciii = chr((ord(i) - 97 + num)% 26 + 97 )
+        ressult += asciii
+    print(result)
+main()
